a624c83191d8ce84a683c57469a5643e67599ef8
Vitória <vitro.geek@gmail.com>
2026-03-31 10:37:41 -0300

    feat: Operadores aritiméticos, lógicos e de comparação

---
+++ src/js/script.js
@@ -0,0 +1,24 @@
+// OPERADORES ARITIMÉTICOS
+
+const valor1=20;
+const valor2=10;
+console.log(valor1 + valor2);
+console.log(valor1 - valor2);
+console.log(valor1 / valor2);
+console.log(valor1 * valor2);
+
+// OPERADORES LÓGICOS
+
+console.log(valor1 < valor2);
+console.log(valor1 < 10 && valor2 != 20);
+console.log(valor1 < 5 || valor1 > 50);
+
+// OPERADORES DE COMPARAÇÃO
+
+console.log(valor1 < valor2);
+console.log(valor1 >= valor2);
+console.log(valor1 =< valor2);
+console.log(valor1 != valor2);
+console.log(valor1 == valor2); //comparação
+console.log(valor1 === valor2); // comparação e verificação de tipo
+console.log("O valor é", valor1);
