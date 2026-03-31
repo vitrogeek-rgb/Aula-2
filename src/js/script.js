d7088b09414b881bae5d80608f86e7eda4178ef0
Vitória <vitro.geek@gmail.com>
2026-03-31 11:08:40 -0300

    feat:Exercicio, operadores

--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,41 @@
-// OPERADORES ARITIMÉTICOS
+// // OPERADORES ARITIMÉTICOS
 
-const valor1=20;
-const valor2=10;
-console.log(valor1 + valor2);
-console.log(valor1 - valor2);
-console.log(valor1 / valor2);
-console.log(valor1 * valor2);
+// const valor1=20;
+// const valor2=10;
 
-// OPERADORES LÓGICOS
+// console.log(valor1 + valor2);
+// console.log(valor1 - valor2);
+// console.log(valor1 / valor2);
+// console.log(valor1 * valor2);
 
-console.log(valor1 < valor2);
-console.log(valor1 < 10 && valor2 != 20);
-console.log(valor1 < 5 || valor1 > 50);
+// // OPERADORES LÓGICOS
 
-// OPERADORES DE COMPARAÇÃO
+// console.log(valor1 < valor2);
+// console.log(valor1 < 10 && valor2 != 20);
+// console.log(valor1 < 5 || valor1 > 50);
 
-console.log(valor1 < valor2);
-console.log(valor1 >= valor2);
-console.log(valor1 =< valor2);
-console.log(valor1 != valor2);
-console.log(valor1 == valor2); //comparação
-console.log(valor1 === valor2); // comparação e verificação de tipo
-console.log("O valor é", valor1);
+// // OPERADORES DE COMPARAÇÃO
+
+// console.log(valor1 < valor2);
+// console.log(valor1 >= valor2);
+// console.log(valor1 <= valor2);
+// console.log(valor1 != valor2);
+// console.log(valor1 == valor2); //comparação
+// console.log(valor1 === valor2); // comparação e verificação de tipo
+// console.log("O valor é", valor1);
+
+//EXERCICIO 1
+const p= 200;
+const v= 20;
+const precoFinal= p - v;
+console.log("Valor com desconto:", precoFinal)
+
+
+//EXERCICIO 2
+const login= 1
+const token= 11
+const transacaoAutorizada = login === 1 && token >= 10;
+
+console.log("Transação autorizada com sucesso",transacaoAutorizada)
+
+//EXERCICIO 3
